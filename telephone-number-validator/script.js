bbb6730668de6763b82ef2af931a121e204c40f0
Abraham Galue <abrahamgalue512@gmail.com>
2024-04-12 18:56:55 -0400

    Setting up the js

---
+++ telephone-number-validator/script.js
@@ -0,0 +1,4 @@
+const textInput = document.getElementById('user-input')
+const checkBtn = document.getElementById('check-btn')
+const clearBtn = document.getElementById('clear-btn')
+const results = document.getElementById('results-div')
